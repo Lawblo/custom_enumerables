31011dd1fe576ec573616b431c51d7d3384e72d2
Thorkil <schjodtthorkil@gmail.com>
2021-12-14 20:01:46 +0100

    completed my_none

--- lib/custom_enumerables.rb
+++ lib/custom_enumerables.rb
@@ -27,6 +27,8 @@ module Enumerable
   # Returns an array containing all elements of enum for which the given block returns a true value.
   # Returns hash for hashes
   def my_select
+    return self unless block_given?
+
     case self
     when Array
       store_return = []
@@ -44,20 +46,35 @@ module Enumerable
   # which will cause all? to return true when none of the collection members
   # are false or nil.
   def my_all?
+    self.my_each { |i| return true if i } unless block_given?
     all_true = true
     case self
     when Array
-      my_each do |i|
-        all_true = false unless yield i
-      end
+      my_each { |i| all_true = false unless yield i }
     when Hash
-      my_each do |i|
-        #puts "#{i[1]} is a #{i[1].class}"
-        all_true = false unless yield i[1]
-      end
+      my_each { |i| all_true = false unless yield i[1] }
     end
     all_true
   end
+
+  # Passes each element of the collection to the given block.
+  # The method returns true if the block ever returns a value other than false or nil.
+  # If the block is not given, Ruby adds an implicit block of { |obj| obj } that will
+  # cause any? to return true if at least one of the collection members is not false or nil.
+  def my_any?
+    my_each { |i| return true if i } unless block_given?
+    my_each  {|i| return true if yield(i)}
+    false
+  end
+
+  def my_none?
+    no_true = true
+    my_each { |i| no_true = false if i } if !block_given?
+    my_each { |i| no_true = false if yield i }
+    no_true
+  end
 end
 
-TestMethods.new.test_my_all
+TestMethods.new.test_my_none
+
+
--- lib/test_methods.rb
+++ lib/test_methods.rb
@@ -7,7 +7,8 @@ class TestMethods
 
   def initialize
     @hash = {a: 'first', b: 'second', c: 3}
-    @arr = [1, 2, 3, 'a', 'b', 'c']
+    @arr = [1, 2, 3]
+    # @arr = [nil, nil, nil]
   end
 
   def test_my_select
@@ -26,5 +27,19 @@ class TestMethods
     p(hash.my_all?  { |k, v| k == String })
     p(hash.all?     { |k, v| k != String })
     p(hash.my_all?  { |k, v| k != String })
+  end
+
+  def test_my_any
+    p(arr.any?      { |i| i == 1})
+    p(arr.my_any?   { |i| i == 1})
+    p(hash.any?     { |k, v| v == 'first'})
+    p(hash.my_any?  { |k, v| v == 'second'})
+  end
+
+  def test_my_none
+    # p(arr.none?     { |i| i == 4})
+    # p(arr.my_none?  { |i| i == 4})
+    p(hash.none?     { |k, v| v == 'first'})
+    p(hash.my_none?  { |k, v| v == 'second'})
   end
 end
